3b6e4dc2c082bd93345fd8c1c51aebfac3740fca
roman-diaz <romdiaz1@gmail.com>
2019-09-06 21:31:53 -0300

    first commit

---
+++ base_flask.py
@@ -0,0 +1,9 @@
+from Flask import flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return render_template('home.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
